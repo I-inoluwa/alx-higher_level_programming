e0120f99668640e274a4abb56e12c3416cad9d1e
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-04 02:02:57 +0100

    Task 1 commit. extra

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 
 last = abs(number) % 10
-new_last = last
 if (number < 0):
-    new_last = -1 * last
+    last = -1 * last
 if last > 5:
     literal = "and is greater than 5"
 elif last == 0:
@@ -13,4 +12,4 @@ elif last == 0:
 else:
     literal = "and is less than 6 and not 0"
 
-print(f"Last digit of {number} is {new_last} {literal}")
+print(f"Last digit of {number} is {last} {literal}")
