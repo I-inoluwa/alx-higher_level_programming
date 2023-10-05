5311cf31263de7e883f35e8f8c25bff1e59e224d
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-05 01:58:39 +0100

    Interview prep.

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node into a sorted linked list
+ * @head: sorted linked list;
+ * @number: value of node to be inserted;
+ * Return: returns the node, else NULL.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = NULL, *current = NULL;
+
+	current = *head;
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->n = number;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (current->next != NULL)
+	{
+		if ((current->next)->n > number)
+		{
+			node->next = current->next;
+			current->next = node;
+			return (node);
+		}
+		current = current->next;
+	}
+
+	current->next = node;
+	return (node);
+}
