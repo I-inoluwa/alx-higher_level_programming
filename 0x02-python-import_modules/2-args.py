41d165f12941a2040c80b93b4b082efe0b0692f8
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 00:44:18 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 def main():
     from sys import argv
-    
+
     length = len(argv)
     ends = ":\n"
     if (length - 1) == 1:
@@ -11,7 +11,7 @@ def main():
         if (length - 1) == 0:
             ends = ".\n"
         print("{:d} arguments".format(length - 1), end=ends)
-    
+
     for i in range(1, length):
         print("{:d}: {}".format(i, argv[i]))
 
