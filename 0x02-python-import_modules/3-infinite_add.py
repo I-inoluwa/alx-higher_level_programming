1c7cb0b5758f3dbbe9f4d373c41c04e1ccac0e63
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 00:47:00 +0100

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def main():
+    from sys import argv
+
+    length = len(argv)
+    sum = 0
+
+    for i in range(1, length):
+        sum += int(argv[i])
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
