ecaa0e0502aa93bd502e1ddea82768f5a115ae2a
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 00:44:00 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+def main():
+    from sys import argv
+    
+    length = len(argv)
+    ends = ":\n"
+    if (length - 1) == 1:
+        print("{:d} argument".format(length - 1), end=ends)
+    else:
+        if (length - 1) == 0:
+            ends = ".\n"
+        print("{:d} arguments".format(length - 1), end=ends)
+    
+    for i in range(1, length):
+        print("{:d}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
