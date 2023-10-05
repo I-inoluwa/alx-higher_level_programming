529abc7cd31a2e56012948ddd7d59b3cda8de141
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-05 02:40:44 +0100

    advanced 2

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+def remove_char_at(str, n):
+    i = 0
+    new_str = ""
+    for c in str:
+        if (i == n):
+            i += 1
+            continue
+        new_str += c
+        i += 1
+
+    return new_str
