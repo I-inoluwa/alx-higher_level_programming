13411f4e99deef6a1ad32087a82cab51e60fb831
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 02:40:57 +0100

    Magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import dis
 def magic_calculator(a, b):
     from magic_calculation_102 import add, sub
     if (a < b):
@@ -6,4 +7,7 @@ def magic_calculator(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-    return (sub(a, b))
+
+    return(sub(a,b))
+
+dis.dis(magic_calculator)
