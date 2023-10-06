cfefeeb912fa91cf075fa8534a5e9b9b5bc4e2c8
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 01:24:42 +0100

    Task 4 commit.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+import hidden_4
+
+
+def main():
+    hidden_funcs = dir(hidden_4)
+
+    for each in sorted(hidden_funcs):
+        if each[:2] != "__":
+            print(each)
+
+
+if __name__ == "__main__":
+    main()
