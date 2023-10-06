0406dd26f2fb519ac2ffbccf399e82d5225b6fdf
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-06 01:51:34 +0100

    Advanced 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+
+def main():
+    length = len(argv)
+
+    if (length - 1 != 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    elif op == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{:d} {} {:d} = {:d}".format(a, op, b, result))
+
+
+if __name__ == "__main__":
+    main()
