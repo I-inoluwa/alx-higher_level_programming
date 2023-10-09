9d8f8a82deb7926574c1cfa8bab8197c3ca1ebce
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 15:25:03 +0100

    task 1 re

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
     i = 0
     for each in my_list:
         if (i == idx):
             return (each)
         i += 1
-
+    
     return(None)
