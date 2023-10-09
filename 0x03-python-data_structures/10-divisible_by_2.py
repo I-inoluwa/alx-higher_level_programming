d882f01602c6cd350aad5d5bb92afc15d09379c5
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 17:39:01 +0100

    def divisible_by_2(my_list=[]):

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-
+def divisible_by_2(my_list=[]):
+    if (my_list):
+        new = []
+        for each in my_list:
+            if (each % 2 == 0):
+                new.append(True)
+            else:
+                new.append(False)
+        return (new)
+    return (my_list)
