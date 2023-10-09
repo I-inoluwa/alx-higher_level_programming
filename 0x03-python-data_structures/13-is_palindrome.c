826f60e33d97a0aec67c2a72ee83b6ef553c46ac
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 21:06:48 +0100

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome;
+ * @head: linked list to be checked;
+ * Return: returns (1) if list is a palindrome, and (0) otherwise.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *cur = NULL, *pal = NULL, *new = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	cur = *head;
+	while (cur != NULL)
+	{
+		pal = malloc(sizeof(listint_t));
+		if (pal == NULL)
+			return (0);
+		pal->n = cur->n;
+		pal->next = NULL;
+
+		cur = cur->next;
+		if (new == NULL)
+		{
+			new = pal;
+			continue;
+		}
+		pal->next = new;
+		new = pal;
+	}
+	cur = *head;
+	pal = new;
+	while (cur != NULL)
+	{
+		if (cur->n != pal->n)
+		{
+			free_listint(new);
+			return (0);
+		}
+		cur = cur->next;
+		pal = pal->next;
+	}
+	free_listint(new);
+	return (1);
+}
