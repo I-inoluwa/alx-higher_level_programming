a6a48aa87b8daa94ee11239500e56697264c40de
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 18:04:33 +0100

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
-
+def delete_at(my_list=[], idx=0):
+    if (my_list):
+        i = 0
+        j = -1
+        new_list = []
+        length = len(my_list)
+        for i in range(length):
+            if (i == idx):
+                j = i
+            else:
+                new_list.append(my_list[i])
+        if (j >= 0):
+            del (my_list[j])
+        return (new_list)
+    return (my_list)
