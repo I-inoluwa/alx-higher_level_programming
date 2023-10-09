e8c98347df98f04b641e0084f1fb5fce8da7809a
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 16:52:00 +0100

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+def no_c(my_string):
+    if my_string:
+        new = ""
+        for i in range(len(my_string)):
+            if my_string[i] not in "cC":
+                new += my_string[i]
+
+        return (new)
+    return (my_string)
