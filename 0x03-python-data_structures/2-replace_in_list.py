6d5eea195f83429b2d6f47924d95f6d8b960136f
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 16:07:40 +0100

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+def replace_in_list(my_list, idx, element):
+    i = 0
+    for each in my_list:
+        if (i == idx):
+            my_list[i] = element
+        i += 1
+
+    return (my_list)
