4d3c5bad337d3e9d7aa22b200a4870ec0c01d27b
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 17:34:29 +0100

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-
+def max_integer(my_list=[]):
+    if (my_list):
+        max = my_list[0]
+        for each in my_list:
+            if each > max:
+                max = each
+        return (max)
+    return (None)
