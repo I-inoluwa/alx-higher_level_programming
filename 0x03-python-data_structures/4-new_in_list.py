74d34fbdef0a902909fe0f8549d4182fdf3131bf
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-09 16:39:06 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+def new_in_list(my_list, idx, element):
+    if (my_list):
+        new_list = []
+        i = 0
+        for each in my_list:
+            new_list.append(my_list[i])
+            if (i == idx):
+                new_list[i] = element
+            i += 1
+
+        return (new_list)
+    return (my_list)
