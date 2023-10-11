00b4020f3e231ac27c3ee9c81a9d46a7b598fe2a
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 23:30:52 +0100

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    if (set_1 and set_2):
+        new = set_1.difference(set_2).union(set_2.difference(set_1))
+        return (new)
+
+    elif set_1:
+        return set_1
+
+    elif set_2:
+        return set_2
+
+    return (set())
