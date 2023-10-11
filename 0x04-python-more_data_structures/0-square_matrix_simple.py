af5eab3d9f5851f3e13ecdffad53ac49a3cbf169
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 22:13:57 +0100

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = []
+        for each in matrix:
+            row = [x**2 for x in each]
+            new_matrix.append(row)
+
+        return (new_matrix)
+    return (matrix)
