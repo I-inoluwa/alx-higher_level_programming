8223be9b5c2bee96e585db5b1774dcaad6f25953
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 22:29:42 +0100

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = [replace if x == search else x for x in my_list]
+        return new_list
+    return my_list
