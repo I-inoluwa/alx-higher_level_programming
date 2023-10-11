885131a7d1c8a6178d93eac33d83419fc063b4ab
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 23:41:46 +0100

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    if (a_dictionary):
+        num = 0
+        for each in a_dictionary:
+            num += 1
+        return (num)
+    return (0)
