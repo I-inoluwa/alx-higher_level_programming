f4794f4e094a1d199c35335beac5b242d0d5b44a
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 23:20:19 +0100

    3-common_elements.py again

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,10 +3,6 @@
 def common_elements(set_1, set_2):
     if (set_1 and set_2):
         new = set(set_1.intersection(set_2))
+        # new = set([x for x in set_1 if x in set_2])
         return (new)
-
-    elif (set_1):
-        return (set_1)
-
-    elif (set_2):
-        return (set_2)
+    return (set())
