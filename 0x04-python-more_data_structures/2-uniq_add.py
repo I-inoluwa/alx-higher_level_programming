10fd0ba3eb330ad80f7dc1f1ded03b939d0c2594
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-11 23:06:28 +0100

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    if my_list:
+        new_list = set(my_list)
+        result = 0
+        for each in new_list:
+            result += each
+        return (result)
+    return (0)
