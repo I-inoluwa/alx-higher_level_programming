3b91625f142e4e9547e5f13c29e1901dc2e33bb3
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-12 01:49:30 +0100

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if (a_dictionary):
+        for each in sorted(a_dictionary.keys()):
+            print("{}: {}".format(each, a_dictionary[each]))
