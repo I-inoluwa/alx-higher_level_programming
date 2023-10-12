fd8bc98cd206109335ca9273c506e3897278888b
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-12 05:22:24 +0100

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if (my_list):
+        score = [x[0] * x[1] for x in my_list]
+        result = 0
+        weight = 0
+        for i, each in enumerate(score):
+            result += each
+            weight += my_list[i][1]
+
+        return (result / weight)
+    return (0)
