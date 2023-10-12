bd5e8a58d63aeabf3e1e47eb2120a98b6cf58002
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-12 05:11:51 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+from functools import reduce
+
+
+def roman_to_int(roman_string):
+    if (type(roman_string) == str):
+        cache = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        track = [10000]
+        for each in roman_string.upper():
+            # track.append(cache[each])
+            if (track[-1] < cache[each]):
+                track[-1] = cache[each] - track[-1]
+            else:
+                track.append(cache[each])
+
+        result = reduce(lambda x, y: x + y, track[1:])
+        return (result)
+    return (0)
