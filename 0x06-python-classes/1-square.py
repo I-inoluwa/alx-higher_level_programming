dfa2360adfc174f5971c6f0f359908a3393f5616
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-25 00:14:51 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""Oh! This needs a documentation too."""
 
+
+class Square:
+    """Creates a square with an attribute."""
+
+    def __init__(self, size):
+        """Initializes a square with a size attribute
+
+        Attributes:
+            size: The size of the square
+        """
+
+        self._Square__size = size
