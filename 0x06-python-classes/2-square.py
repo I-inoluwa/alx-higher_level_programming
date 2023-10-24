516faaea68218f0f50cdbc88db730cfa8e5b97ee
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-25 00:30:39 +0100

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""Oh! This needs a documentation too."""
 
+
+class Square:
+    """Creates a square with an attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a square with a size attribute
+
+        Attributes:
+            size: The size of the square. Initialised to 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+        self._Square__size = size
