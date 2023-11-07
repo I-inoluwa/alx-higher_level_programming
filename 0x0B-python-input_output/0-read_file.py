66f8f729e0cec053aa9aabd46aad1b83fbe69c99
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-07 20:36:36 +0100

    Task 0 fixed.:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ def read_file(filename=""):
     """A function that reads a text file, and prints it to stdout"""
 
     with (open(filename, encoding="utf-8") as f):
-        lines = f.readlines()
-    for each in lines:
-        print(each, end="")
+        for each in f:
+            print(each, end="")
