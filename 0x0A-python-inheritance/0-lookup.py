3c1f3990acead2e688b07e5b9775b01033320c3f
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-07 04:09:39 +0100

    Commit 2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This documentation is always important"""
+
+
+def lookup(obj):
+    """Function that returns all methods and attributes of an object"""
+
+    return (dir(object))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This documentation is always important"""
+
+
+class MyList(list):
+    """A new class that inherits from python's inherent list"""
+
+    def print_sorted(self):
+        """Function that prints a list in its sorted form"""
+        print(sorted(self))
