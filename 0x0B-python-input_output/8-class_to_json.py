0cef89a8e6ebda9b76b2cf3e348aaebf1879eb78
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-07 23:26:25 +0100

    temp fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,26 +5,6 @@
 def class_to_json(obj):
     """Returns the dictionary description for JSON of an object"""
     serial = {}
-    for each in dir(obj):
+    for each in vars(obj):
         serial[each] = obj.__getattribute__(each)
     return serial
-
-class MyClass:
-    """ My class
-    """
-
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
-
-    def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
-    
-m = MyClass("John")
-m.number = 89
-print(type(m))
-print(m)
-
-mj = class_to_json(m)
-print(type(mj))
-print(mj)
