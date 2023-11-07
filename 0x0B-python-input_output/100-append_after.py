c34937f95a254fb9b166a837c06d575ed9802f08
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-07 23:23:07 +0100

    partial fixes

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 """This is the documentation for the module. Necessary"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Appends to a file (or inserts in the line) after a string is found """
+
+    with open(filename, mode='r', encoding='utf-8') as f:
+        content = f.readlines()
+        for i, line in enumerate(content):
+            if search_string in line:
+                content.insert(i + 1, new_string)
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.writelines(content)
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
 """This is the documentation for the module. Necessary"""
+
+
+def stdin_read():
+    """A function that reads form stdin line by line and performs an op"""
+    count = 0
+    total = 0
+    possible = [200, 301, 400, 401, 403, 404, 405, 500]
+    pr_list = {x: 0 for x in possible}
+    while (1):
+        line_arr = input().split("\"GET /projects/260 HTTP/1.1\"")
+        # print(line_arr)
+        line_arr = line_arr[1].split()
+        stc = int(line_arr[0])
+        fls = int(line_arr[1])
+        total += fls
+        if count % 10 == 0:
+            for each in sorted(pr_list.keys()):
+                if pr_list[each] == 0:
+                    continue
+                print(f"{each:d} : {pr_list[each]:d}")
+            print("File size: {:d}".format(total))
+        if stc in possible:
+            pr_list[stc] += 1
+        count += 1
+
+
+stdin_read()
