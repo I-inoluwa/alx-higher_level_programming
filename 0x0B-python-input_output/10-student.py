886ad29c36fcd86e6734cb2432f09f865bdc87dd
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 00:04:08 +0100

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 """This is the documentation for the module. Necessary"""
+
+
+class Student:
+    """Creation of a Student class, with some attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialiser of an instance of the Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrives a dict repr of a Student instance"""
+        serial = {}
+        flag = 0
+        if attrs and type(attrs) == list and type(attrs[0]) == str:
+            flag = 1
+        for each in vars(self):
+            if flag == 1:
+                if each in attrs:
+                    serial[each] = self.__getattribute__(each)
+            else:
+                serial[each] = self.__getattribute__(each)
+
+        return serial
