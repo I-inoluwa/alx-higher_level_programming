faeba2677456ea44de5263213ec8910d1e3c1176
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 01:15:53 +0100

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,20 +6,37 @@ count = 0
 total = 0
 possible = [200, 301, 400, 401, 403, 404, 405, 500]
 pr_list = {x: 0 for x in possible}
-while (1):
-    line_arr = input().split("\"GET /projects/260 HTTP/1.1\"")
-    # print(line_arr)
-    line_arr = line_arr[1].split()
-    stc = int(line_arr[0])
-    fls = int(line_arr[1])
-    total += fls
-    if count % 10 == 0:
-        for each in sorted(pr_list.keys()):
-            if pr_list[each] == 0:
-                continue
-            print(f"{each:d} : {pr_list[each]:d}")
 
-        print("File size: {:d}".format(total))
-    if stc in possible:
-        pr_list[stc] += 1
-    count += 1
+try:
+    while True:
+        line = input()
+        line_arr = line.split("\"GET /projects/260 HTTP/1.1\"")
+        if len(line_arr) < 2:
+            continue
+
+        line_arr = line_arr[1].split()
+        if len(line_arr) < 2:
+            continue
+
+        stc = int(line_arr[0])
+        fls = int(line_arr[1])
+        total += fls
+        if count % 10 == 0:
+            for each in sorted(pr_list.keys()):
+                if pr_list[each] == 0:
+                    continue
+                print(f"{each:d} : {pr_list[each]:d}")
+
+            print("File size: {:d}".format(total))
+
+        if stc in possible:
+            pr_list[stc] += 1
+        count += 1
+
+except KeyboardInterrupt:
+    for each in sorted(pr_list.keys()):
+        if pr_list[each] == 0:
+            continue
+        print(f"{each:d} : {pr_list[each]:d}")
+
+    print("File size: {:d}".format(total))
