132fd5145fa207bb671fb6edc504fd8eec18492f
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 03:52:01 +0100

    alx

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,41 +4,41 @@ I honestly do not know what is failing at this point.
 """
 
 
-count = 0
-total = 0
-possible = [200, 301, 400, 401, 403, 404, 405, 500]
-pr_list = {x: 0 for x in possible}
+# count = 0
+# total = 0
+# possible = [200, 301, 400, 401, 403, 404, 405, 500]
+# pr_list = {x: 0 for x in possible}
 
-try:
-    while True:
-        line = input()
-        line_arr = line.split("\"GET /projects/260 HTTP/1.1\"")
-        if len(line_arr) < 2:
-            continue
+# try:
+#     while True:
+#         line = input()
+#         line_arr = line.split("\"GET /projects/260 HTTP/1.1\"")
+#         if len(line_arr) < 2:
+#             continue
 
-        line_arr = line_arr[1].split()
-        if len(line_arr) < 2:
-            continue
+#         line_arr = line_arr[1].split()
+#         if len(line_arr) < 2:
+#             continue
 
-        stc = int(line_arr[0])
-        fls = int(line_arr[1])
-        total += fls
-        if count % 10 == 0:
-            for each in sorted(pr_list.keys()):
-                if pr_list[each] == 0:
-                    continue
-                print(f"{each:d} : {pr_list[each]:d}")
+#         stc = int(line_arr[0])
+#         fls = int(line_arr[1])
+#         total += fls
+#         if count % 10 == 0:
+#             for each in sorted(pr_list.keys()):
+#                 if pr_list[each] == 0:
+#                     continue
+#                 print(f"{each:d} : {pr_list[each]:d}")
 
-            print("File size: {:d}".format(total))
+#             print("File size: {:d}".format(total))
 
-        if stc in possible:
-            pr_list[stc] += 1
-        count += 1
+#         if stc in possible:
+#             pr_list[stc] += 1
+#         count += 1
 
-except KeyboardInterrupt:
-    for each in sorted(pr_list.keys()):
-        if pr_list[each] == 0:
-            continue
-        print(f"{each:d} : {pr_list[each]:d}")
+# except KeyboardInterrupt:
+#     for each in sorted(pr_list.keys()):
+#         if pr_list[each] == 0:
+#             continue
+#         print(f"{each:d} : {pr_list[each]:d}")
 
-    print("File size: {:d}".format(total))
+#     print("File size: {:d}".format(total))
