bce45e08903165847bd01602450904025c2bbeb3
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 01:01:43 +0100

    100-append_after.py confim

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,13 +6,6 @@ def append_after(filename="", search_string="", new_string=""):
     """Appends to a file (or inserts in the line) after a string is found """
 
     with open(filename, mode='r', encoding="utf-8") as f:
-        # content = f.readlines()
-        # for i, line in enumerate(content):
-        #     if search_string in line:
-        #         if line[-1] != '\n':
-        #             content[i] = line + '\n'
-        #         content.insert(i + 1, new_string)
-
         content = []
         for line in f:
             content.append(line)
