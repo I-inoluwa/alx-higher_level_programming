2801ff6b53c7732a12da1ee28e88fba669b7240e
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 03:55:35 +0100

    alxst

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,12 +9,12 @@ total = 0
 possible = [200, 301, 400, 401, 403, 404, 405, 500]
 pr_list = {x: 0 for x in possible}
 
-# try:
-#     while True:
-#         line = input()
-#         line_arr = line.split("\"GET /projects/260 HTTP/1.1\"")
-#         if len(line_arr) < 2:
-#             continue
+try:
+    while True:
+        line = input()
+        line_arr = line.split("\"GET /projects/260 HTTP/1.1\"")
+        if len(line_arr) < 2:
+            continue
 
 #         line_arr = line_arr[1].split()
 #         if len(line_arr) < 2:
@@ -35,10 +35,11 @@ pr_list = {x: 0 for x in possible}
 #             pr_list[stc] += 1
 #         count += 1
 
-# except KeyboardInterrupt:
-#     for each in sorted(pr_list.keys()):
-#         if pr_list[each] == 0:
-#             continue
-#         print(f"{each:d} : {pr_list[each]:d}")
+except KeyboardInterrupt:
+    # for each in sorted(pr_list.keys()):
+    #     if pr_list[each] == 0:
+    #         continue
+    #     print(f"{each:d} : {pr_list[each]:d}")
 
-#     print("File size: {:d}".format(total))
+    # print("File size: {:d}".format(total))
+    pass
