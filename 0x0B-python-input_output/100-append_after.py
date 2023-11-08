2a37c134b3dd19e4f18f2a118660b0a8c5f97f22
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 01:00:29 +0100

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,12 +5,19 @@
 def append_after(filename="", search_string="", new_string=""):
     """Appends to a file (or inserts in the line) after a string is found """
 
-    with open(filename, mode='r', encoding='utf-8') as f:
-        content = f.readlines()
-        for i, line in enumerate(content):
+    with open(filename, mode='r', encoding="utf-8") as f:
+        # content = f.readlines()
+        # for i, line in enumerate(content):
+        #     if search_string in line:
+        #         if line[-1] != '\n':
+        #             content[i] = line + '\n'
+        #         content.insert(i + 1, new_string)
+
+        content = []
+        for line in f:
+            content.append(line)
             if search_string in line:
-                if line[-1] != '\n':
-                    content[i] = line + '\n'
-                content.insert(i + 1, new_string)
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.writelines(content)
+                content.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as g:
+        g.writelines(content)
