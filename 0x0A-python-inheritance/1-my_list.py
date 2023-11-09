7985e8b8495e35c8e804a9c041e19dce2408a89c
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-09 01:16:15 +0100

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@
 class MyList(list):
     """A new class that inherits the python's inherent list"""
 
+    def __init__(self) -> None:
+        return super().__init__()
+
     def print_sorted(self):
         """Function that prints a list in its sorted form"""
         print(sorted(self))
