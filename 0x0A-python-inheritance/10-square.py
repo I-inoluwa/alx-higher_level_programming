d4be43f4fb390d5dc79c372bfabeba788e567c7c
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-13 21:34:39 +0100

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,47 +2,8 @@
 """Module for creating an empty class, with a strange method."""
 
 
-class BaseGeometry:
-    """Empty BaseGeometry class. Area method is added."""
-
-    def __init__(self):
-        """Initialization of an instance of a class"""
-        pass
-
-    def area(self):
-        """Class method for finding the area of an object"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Class method for validating a specific attribute"""
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class that inherits 4rm BaseGeometry class"""
-
-    def __init__(self, width, height):
-        """Initialisation of the Rectangle class."""
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self._Rectangle__height = height
-        self._Rectangle__width = width
-
-    def area(self):
-        """Area is now being implemented"""
-
-        return (self._Rectangle__height * self._Rectangle__width)
-
-    def __str__(self):
-        """A string representation of rectangle object"""
-        w = self._Rectangle__width
-        h = self._Rectangle__height
-        return ("[Rectangle] {}/{}".format(w, h))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
