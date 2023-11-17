d9024f5be0b59841d623a6126b9110ad192023bb
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-17 23:36:41 +0100

    ../14-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Module Documentation: Base file for all other models"""
 
 
+import json
+
+
 class Base:
     """Base class. All other classes inherit from this."""
 
@@ -14,3 +17,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Converts a dictionary to JSON string"""
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return "[]"
