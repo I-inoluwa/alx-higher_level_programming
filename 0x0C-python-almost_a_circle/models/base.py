151be805c337f9bc516b13f5b1763cfc3d0ac22a
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-17 13:23:30 +0100

    fixed pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """Instantiation of the base class"""
-        if (id != None):
+        if (id is not None):
             self.id = id
         else:
             Base.__nb_objects += 1
