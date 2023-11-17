15c0f5ed49687430575a6bd077c750ca9d75b5a0
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-18 00:01:31 +0100

    ../15-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,21 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Converts a dictionary to JSON string"""
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string repr of list_objs to a file"""
+        arr = []
+        for each in list_objs:
+            arr.append(each.to_dictionary())
+        json_repr = cls.to_json_string(arr)
+
+        filename = f"{str(list_objs[0]).split()[0][1:-1]}.json"
+        with open(filename, mode='w', encoding='utf-8') as f:
+            f.write(json_repr)
